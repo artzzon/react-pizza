89ecc30b0264577376da07714b2a2a25fcd2da07
artzzon <nexus.artem@gmail.com>
2024-01-21 23:47:32 +0300

    add choose active category

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,13 +1,35 @@
+import React from "react";
+
 function Categories() {
+  const categories = [
+    "Все",
+    "Мясные",
+    "Вегетарианские",
+    "Гриль",
+    "Острые",
+    "Закрытые",
+  ];
+  const [activeCategory, setActiveCategory] = React.useState("Все");
+
+  const onClickActiveCategory = (e) => {
+    const category = e.target.innerText;
+    if (category !== activeCategory) {
+      setActiveCategory(category);
+    }
+  };
+
   return (
     <div className="categories">
       <ul>
-        <li className="active">Все</li>
-        <li>Мясные</li>
-        <li>Вегетарианская</li>
-        <li>Гриль</li>
-        <li>Острые</li>
-        <li>Закрытые</li>
+        {categories.map((category) => (
+          <li
+            key={category}
+            className={category === activeCategory ? "active" : ""}
+            onClick={(e) => onClickActiveCategory(e)}
+          >
+            {category}
+          </li>
+        ))}
       </ul>
     </div>
   );
