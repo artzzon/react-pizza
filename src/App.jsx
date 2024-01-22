787e492cbe29b7dd4d47bb070068c9c8c275b716
artzzon <nexus.artem@gmail.com>
2024-01-23 00:11:27 +0300

    link to backend

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,16 @@ import Header from "./components/Header";
 import Categories from "./components/Categories";
 import Sort from "./components/Sort";
 import PizzaBlock from "./components/PizzaBlock";
-import pizzas from "./assets/pizzas.json";
 import "./scss/app.scss";
+import React from "react";
 
 function App() {
-  // fetch(
-  //   "https://file.notion.so/f/f/b3238354-86d5-4ba6-9ad7-eb01112a9acd/36ad4e93-800e-451b-9831-ae6abe1b28ef/pizzas.json?id=e934efcc-4042-481d-9d73-76f227f1696e&table=block&spaceId=b3238354-86d5-4ba6-9ad7-eb01112a9acd&expirationTimestamp=1706032800000&signature=rUHK_uizY3L13j9RXUJuKiU_OBXz7Ld8Dm9eBsRp7MY&downloadName=pizzas.json"
-  // )
-  //   .then((res) => res.json())
-  //   .then((res) => console.log(res));
+  const [items, setItems] = React.useState([]);
+  React.useEffect(() => {
+    fetch("https://e68369cd08c98611.mokky.dev/items")
+      .then((res) => res.json())
+      .then((json) => setItems(json));
+  }, []);
   return (
     <div className="wrapper">
       <Header />
@@ -22,7 +23,7 @@ function App() {
           </div>
           <h2 className="content__title">Все пиццы</h2>
           <div className="content__items">
-            {pizzas.map((pizza) => (
+            {items.map((pizza) => (
               <PizzaBlock key={pizza.id} {...pizza} />
             ))}
           </div>
