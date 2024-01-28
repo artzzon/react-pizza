35c975a0c8a70a197ab6fbfefddc422e5b513d82
artzzon <nexus.artem@gmail.com>
2024-01-28 22:48:07 +0300

    add clear button on search field

--- src/components/Search/index.jsx
+++ src/components/Search/index.jsx
@@ -3,18 +3,43 @@ import styles from "./Search.module.scss";
 import { SearchContext } from "../../App";
 
 export default function Search() {
-  const { seachValue, setSearchValue } = React.useContext(SearchContext);
+  const { searchValue, setSearchValue } = React.useContext(SearchContext);
 
   const onChangeValue = (e) => {
     setSearchValue(e.target.value);
   };
   return (
-    <input
-      type="text"
-      className={styles.root}
-      placeholder="Поиск пицц..."
-      value={seachValue}
-      onChange={(e) => onChangeValue(e)}
-    />
+    <div className={styles.root}>
+      <input
+        type="text"
+        className={styles.input}
+        placeholder="Поиск пицц..."
+        value={searchValue}
+        onChange={(e) => onChangeValue(e)}
+      />
+      {searchValue && (
+        <svg
+          className={styles.icon}
+          onClick={() => setSearchValue("")}
+          viewBox="0 0 24 24"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <rect width="24" height="24" fill="white" />
+          <path
+            d="M7 17L16.8995 7.10051"
+            stroke="#000000"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          />
+          <path
+            d="M7 7.00001L16.8995 16.8995"
+            stroke="#000000"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          />
+        </svg>
+      )}
+    </div>
   );
 }
